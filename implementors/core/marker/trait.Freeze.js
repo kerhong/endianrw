88796293c4435ae113591f38994d9c2ce83cbada
Travis CI User <travis@example.org>
2020-08-01 13:06:14 +0000

    Documentation for kerhong/endianrw@cdc8596

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["endianrw"] = [{"text":"impl Freeze for <a class=\"enum\" href=\"endianrw/enum.BigEndian.html\" title=\"enum endianrw::BigEndian\">BigEndian</a>","synthetic":true,"types":["endianrw::BigEndian"]},{"text":"impl Freeze for <a class=\"enum\" href=\"endianrw/enum.LittleEndian.html\" title=\"enum endianrw::LittleEndian\">LittleEndian</a>","synthetic":true,"types":["endianrw::LittleEndian"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
